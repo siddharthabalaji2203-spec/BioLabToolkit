96a008305ba8afd974b368932368ab3c71e68017
Spectrax <siddharthabalaji2203@gmail.com>
2026-07-07 11:34:06 +0530

    Fasta reader online

---
+++ DNA/fasta_reader.py
@@ -0,0 +1,20 @@
+with open("../Data/sample.fasta", "r") as file:
+    lines = file.readlines()
+
+header = lines[0].replace(">", "").strip()
+
+sequence_lines = lines[1:]
+
+sequence = "".join(sequence_lines)
+
+sequence = sequence.replace("\n", "")
+
+length = len(sequence)
+
+print("========== FASTA READER ==========")
+print()
+print("Header:", header)
+print()
+print("Sequence:", sequence)
+print()
+print("Length:", length, "bp")
